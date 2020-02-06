a4032d8fa6250c80dca28dfb0ee7bb12cbfb55e7
hieutrann <hieu.nghiem@okstate.edu>
2020-02-06 06:37:42 -0600

    1st commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  template:`<div>
+  <h1>
+    Welcome all {{pageTitle}}!!
+  </h1>
+  <pm-products></pm-products>
+</div>
+`
+})
+export class AppComponent {
+  pageTitle: string = 'Acme Product Management';
+
+}
